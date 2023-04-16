6bdf58fa8c8eb130e20de49bb1c2b7624a66a059
dnaurecki <dnaurecki@gmail.com>
2023-04-16 18:57:29 +0200

    first commit

--- build/CMakeFiles/falloutlib.dir/src/utils.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/falloutlib.dir/src/utils.o: \
- /home/rein/Syncthing/git_projects/fallout-lib/src/utils.cpp \
- /usr/include/stdc-predef.h \
- /home/rein/Syncthing/git_projects/fallout-lib/include/utils.h
